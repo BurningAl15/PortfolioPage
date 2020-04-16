bf7a40ad1ed2821d8431af6986fffe5acf89cf66
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2020-04-15 20:09:15 -0500

    init

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-9/12 mx-auto text-center">
+    <h1 class="text-white text-6xl pt-10">
+      Aldhair Vera
+      <br />Camacho
+    </h1>
+    <h2 class="text-red-600 text-4xl pt-6">
+      <i>Game Developer and Front-End Developer</i>
+    </h2>
+    <p class="text-white text-4xl pt-6">
+      Computer Science Student
+      <br />Unity3D / C# / VR
+      <br />HTML / CSS / React JS / Vue JS
+    </p>
+    <SocialMediaBar/>
+  </div>
+</template>
+
+<script>
+import SocialMediaBar from "./SocialMedia";
+
+export default {
+  name: "Hero",
+  components: { SocialMediaBar }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2 class="text-white text-4xl pt-10">Find me on</h2>
+    <div class="flex justify-center h-8 mt-10 h-12">
+      <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+      <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+      <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+      <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+          <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+      <a class="mx-4" href="/" alt="...">
+        <img class="h-24 w-24" :src="facebookImage" />
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import facebookImage from "../Image/SocialNetwork/Facebook.svg";
+export default {
+  data: function() {
+    return {
+      facebookImage
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
